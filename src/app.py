e9e4a0f1b74081066e4b469b5f17e4283575dbb5
Hinostroza <oscarmanuhr@gmail.com>
2026-06-19 21:44:08 -0500

    Refactor: Uso de variables de entorno y limpieza de credenciales/base de datos del repositorio

--- src/app.py
+++ src/app.py
@@ -3,12 +3,57 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify, s
 from pedido import calcular_total
 from models import db, Pedido, Producto  # aqui se importa la base de datos y el modelo ojo al piojo
 from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+#Estas bellezas son las que hacen que google OAuth funcione UWU 
+import requests
+from urllib.parse import urlencode
+#para las variable de entorno UWU 
+from dotenv import load_dotenv
 
 # Configuración de la aplicación y la ruta de las plantillas
 base_dir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__, template_folder='../templates')
-#llave secreta muy secretita
-app.secret_key = 'super_llave_secreta_ingeniosnack_2026'
+# Encendemos la lectura del archivo .env oculto
+load_dotenv()
+
+#Llave secreta muy secretita (ahora oculta de los hackers)
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
+
+# === LLAVES DE GOOGLE === secretito secreton JIJIJIJIJIJIJI
+GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
+# ==========================================
+# CONFIGURACIÓN DE SEGURIDAD (ANTI-CACHÉ)
+# ==========================================
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
+
+# ==========================================
+# GUARDIA DE SEGURIDAD PARA EL ADMIN
+# ==========================================
+def requiere_admin(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'admin_id' not in session:
+            return redirect(url_for('login_admin'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+# ==========================================
+# GUARDIA DE SEGURIDAD PARA EL ESTUDIANTE
+# ==========================================
+def requiere_estudiante(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Si NO hay un estudiante logueado, lo patea al login
+        if 'estudiante_id' not in session:
+            return redirect(url_for('login_estudiante'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 # Configuración de la base de datos SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'ingeniosnack.db')
@@ -50,6 +95,13 @@ class Administrador(db.Model):
 with app.app_context():
     db.create_all()
     
+    if Administrador.query.count() == 0:
+        admin_julio = Administrador(email="admin@ingeniosnack.com")
+        admin_julio.set_password("secreta123")
+        db.session.add(admin_julio)
+        db.session.commit()
+        print("¡Cuenta del Sr. Julio creada con éxito!")
+    
     # Verificamos si la tabla de productos está vacía. Si es así, la llenamos.
     if Producto.query.count() == 0:
         productos_iniciales = [
@@ -78,13 +130,18 @@ with app.app_context():
 
 
 @app.route('/')
+@requiere_estudiante
 def index():
+
+# Si NO inició sesión, lo pateamos al login
+    if 'estudiante_id' not in session:
+        return redirect(url_for('login_estudiante'))
     
-    catalogo_completo = Producto.query.all()
-    
-    return render_template('cliente/index.html', productos=catalogo_completo)
+    productos = Producto.query.filter_by(disponible=True).all()
+    return render_template('cliente/index.html', productos=productos, nombre_alumno=session.get('estudiante_nombre'))
 
 @app.route('/calcular', methods=['POST'])
+@requiere_estudiante
 def calcular():
     # Obtener los datos del formulario HTML
     items_seleccionados = request.form.getlist('producto')
@@ -113,6 +170,7 @@ def calcular():
     return render_template('cliente/exito.html', total=total, ticket=nuevo_pedido.ticket)
 
 @app.route('/admin')
+@requiere_admin
 def admin():   
     # Consultar todos los pedidos de la base de datos (ordenados del más nuevo al más viejo)
     pedidos_reales = Pedido.query.order_by(Pedido.fecha.desc()).all()
@@ -129,12 +187,14 @@ def entregar_pedido(id):
 
 #aqui mero mero se esta poniendo las rutas para el nuevo servidor del panel admin
 @app.route('/admin/inventario')
+@requiere_admin
 def admin_inventario():
     # Traemos todos los productos para que el Sr. Julio pueda prenderlos o apagarlos
     todos_los_productos = Producto.query.all()
     return render_template('admin/inventario.html', productos=todos_los_productos)
 
 @app.route('/admin/toggle_producto/<int:producto_id>', methods=['POST'])
+@requiere_admin
 def toggle_producto(producto_id):
     # Buscamos el producto, si no existe lanza un error 404
     producto = Producto.query.get_or_404(producto_id)
@@ -149,6 +209,7 @@ def toggle_producto(producto_id):
 #========================================Simulacion bien chidori de los productos pq aun no esta en uso y no tenemos conteo real============
 #ruta pa las estadisicas mi parcero
 @app.route('/admin/estadisticas')
+@requiere_admin
 def admin_estadisticas():
     # Simulamos la data de la semana de exámenes parciales
     # Obviamente, el Café es el rey indiscutible de las madrugadas universitarias
@@ -161,6 +222,7 @@ def admin_estadisticas():
 
 #=======================================Se viene otra simulacion pero ahora de clientes porque aun la web no ta en uso pues mi rey============================
 @app.route('/admin/vip')
+@requiere_admin
 def admin_vip():
     # Simulamos la base de datos de usuarios fieles
     clientes_vip = [
@@ -177,11 +239,31 @@ def admin_vip():
 #============================Rutas temporales las borraremos luego recordatorio para mi=================================
 @app.route('/login')
 def login_estudiante():
+    # Si YA inició sesión, lo mandamos al menú
+    if 'estudiante_id' in session:
+        return redirect(url_for('index'))
     return render_template('auth/login_estudiante.html')
 
 @app.route('/login/admin')
 def login_admin():
+    # Si el admin YA inició sesión, lo mandamos a su panel
+    if 'admin_id' in session:
+        return redirect('/admin') 
     return render_template('auth/login_admin.html')
+
+@app.route('/procesar_login_admin', methods=['POST'])
+def procesar_login_admin():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    admin = Administrador.query.filter_by(email=email).first()
+
+    if admin and admin.check_password(password):
+        session['admin_id'] = admin.id
+        session['admin_email'] = admin.email
+        return redirect('/admin')
+    else:
+        return "Error: Credenciales de administrador incorrectas."
 
 # ==========================================
 # RUTAS DE AUTENTICACIÓN
@@ -236,6 +318,70 @@ def logout():
     # Borramos la memoria de la sesión
     session.clear()
     return redirect(url_for('login_estudiante'))
+
+#========Rutas de Autenticación de google OAuth=================
+# --- RUTA 1: ENVIAR A GOOGLE ---
+@app.route('/login/google')
+def login_google():
+    #Si ya inició sesión, lo regresamos al menú
+    if 'estudiante_id' in session:
+        return redirect(url_for('index'))
+
+    params = {
+        'client_id': GOOGLE_CLIENT_ID,
+        'redirect_uri': url_for('callback', _external=True), 
+        'response_type': 'code',
+        'scope': 'openid email profile' 
+    }
+    url_google = f"https://accounts.google.com/o/oauth2/v2/auth?{urlencode(params)}"
+    return redirect(url_google)
+
+
+# --- RUTA 2: RECIBIR DE GOOGLE (CALLBACK) ---
+@app.route('/callback')
+def callback():
+    # SI YA TIENE SESIÓN, NO NECESITA PROCESAR ESTO
+    if 'estudiante_id' in session:
+        return redirect(url_for('index'))
+
+    code = request.args.get('code')
+    if not code:
+        return "Error: Autorización denegada por Google."
+
+    token_url = "https://oauth2.googleapis.com/token"
+    token_data = {
+        'code': code,
+        'client_id': GOOGLE_CLIENT_ID,
+        'client_secret': GOOGLE_CLIENT_SECRET,
+        'redirect_uri': url_for('callback', _external=True),
+        'grant_type': 'authorization_code'
+    }
+    token_response = requests.post(token_url, data=token_data).json()
+    access_token = token_response.get('access_token')
+
+    user_info_url = "https://www.googleapis.com/oauth2/v3/userinfo"
+    headers = {'Authorization': f'Bearer {access_token}'}
+    user_info = requests.get(user_info_url, headers=headers).json()
+
+    email = user_info.get('email')
+    nombre = user_info.get('name')
+
+    estudiante = Estudiante.query.filter_by(email=email).first()
+
+    if not estudiante:
+        codigo_egresado = "EGR-" + email.split('@')[0][:6] 
+        estudiante = Estudiante(
+            nombre_completo=nombre, 
+            email=email, 
+            codigo_matricula=codigo_egresado
+        )
+        db.session.add(estudiante)
+        db.session.commit()
+
+    session['estudiante_id'] = estudiante.id
+    session['estudiante_nombre'] = estudiante.nombre_completo
+    
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
